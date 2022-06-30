4f0e3322351b730655c923876d9d5f09cb3ac089
Manuel Eguia <eguiamc@gmail.com>
2022-06-30 19:39:57 +0200

    maps

--- src/Pluto/Maps backup 1.jl
+++ src/Pluto/Maps backup 1.jl
@@ -4,16 +4,6 @@
 using Markdown
 using InteractiveUtils
 
-# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
-macro bind(def, element)
-    quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
-        local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
-        el
-    end
-end
-
 # ╔═╡ 1097d874-4468-4e76-bdbc-c893a5dbfdc0
 using Plots, PlutoUI
 
@@ -33,152 +23,31 @@ We start studying systems with a single variable $x$. The **space of states** (a
 
 # ╔═╡ 157c155f-241e-4d39-93f7-35997d83191d
 md"""
-## Discrete Time: $x_t \rightarrow x_{t+1}$
+## Discrete Time: $x_n \rightarrow x_{n+1}$
 
-If the time evolution is discrete:
+If the time evolution is discrete we have a **map** and the dynamical system is defined by a rule that assigns a next state of the system ($x_{n+1}$) as a function of the present state ($x_n$) only, and this is denoted by:
 
-$t=0,1,2,3,4,5,6\dots$
+$x_{n+1}=f(x_n)$
 
-we have a **map** and the dynamical system is defined by a rule that assigns a next state of the system at time $t+1$ ($x_{t+1}$) as a function of the present state at time $t$ ($x_t$) only, and this is denoted by:
+Take for example the number of individuals of a population at each generation, and assume that the rate of growth $R$ is fixed. Then the rule of our dynamical system can be written as:
 
-$x_{t+1}=f(x_t)$
+$x_{n+1}=R x_n$
 
-## Simplest example: Continuous Population
+In the animated graphic below 
 
-Take for example the number of individuals of a population at each generation, and assume that the rate of growth $R$ is fixed. 
-Also, assume that since the population is large enough it can be approximated by a continuous value  $x$ (for example in millions).
-Then the rule of our dynamical system can be written as:
-
-$x_{t+1}=R x_t$
-
-In the animated graphic below we can set the initial value of the population $x_0$ and the rate of growth $R$ with the sliders, and also start the iteration of the map controlling the number of seconds for each step (tick)
 
 """
 
-# ╔═╡ 9490e7f8-668e-4acc-8efd-dcc4d355acb9
+# ╔═╡ 541330ac-2212-4681-8abb-f823fa5d1177
+
 md"""
-$(@bind n Clock(1))
-$(@bind reset Button("Reset"))
+The temporal evolution of particular dynamical system can be described by the from an initial state $x(0)$ describing a *trajectory* made by all the states 
+
+The deterministic rule is fixed during the evolution but can depend on one or more parameters.
+
+Much of the study of Dynamical Systems is devoted to study how the temporal evolution of the system changes qualitatively when the parameters change (Bifurcation Theory).
 """
 
-# ╔═╡ d9bb5aa8-2708-40c3-8d42-3b21e6d634c0
-md"""
-x₀ : $(@bind x0 Slider(0.0:0.02:1.0,default=0.1;show_value=true)) \
-R : $(@bind R Slider(0.05:0.01:1.5,default=1.0;show_value=true)) \
-"""
-
-# ╔═╡ dd1af6a9-c048-4845-8858-8051d23f6bee
-md"""
-The upper graphics displays the states of the dynamical system given by the variable $x$ (millions of individuals in the population) for each generation (time step $t$) as red dots in the state space (black line). 
-
-The sequence of red dots ($x_0,x_1,x_2,x_3,x_4,x_5,\dots$) is a **trajectory** of the dynamical system. 
-
-The lower graph displays the value of the variable $x$ as a function of time $t$ as blue dots. This is the **solution** of the system.
-"""
-
-# ╔═╡ 226bcb3d-fdfa-484d-80da-2eb1e778017b
-md"""
-Each initial condition $x_0$ leads to an unique trajectory in the dynamical system.
-
-On the other hand, $R$ is an **external parameter**  that determines the rule of the evolution and does not change over time.
-
-Starting from $x_0$ for generation $t=0$, the population evolves in time either growing without bounds (for $R>1$) or decreasing toward zero (for $R<1$). A marginal case occurs when $R=1$ and the population remains fixed at the initial value. 
-
-Therefore, the parameter $R$ determines the qualitative behavior of the system (population growth or decrease) for all initial conditions, while the initial condition correspond to a particular history of the system (trajectory).
-
-The qualitative theory of dynamical systems is mainly concerned with the possible qualitative changes in the behavior of the systems as the parameters are varied.
-"""
-
-# ╔═╡ a31644b5-3aed-4ff8-9f46-0f8ff407f1b7
-md"""
-$(@bind n2 Clock(1))
-$(@bind reset2 Button("Reset"))
-"""
-
-# ╔═╡ 1115e7e3-06f2-4f51-a71d-85ada76ce3a9
-md"""
-x₀ : $(@bind x02 Slider(0.0:0.02:1.0,default=0.1;show_value=true)) \
-R : $(@bind R2 Slider(0.05:0.01:4.0,default=1.0;show_value=true)) \
-"""
-
-# ╔═╡ 3a6821d7-392a-4dd4-b5c8-c247edf9132f
-mutable struct Map
-	x 
-	t
-	xv
-end	
-
-# ╔═╡ ac175af3-0d5c-43ab-a41f-a5e6dfc1df72
-begin
-	reset
-	m = Map(x0/R,-1,[]);
-end;
-
-# ╔═╡ 7c3fb7dc-47f6-4a6c-b42a-76f74a5bc575
-begin
-	reset2
-	m2 = Map(x02/R2,-1,[]);
-end;
-
-# ╔═╡ 54918524-a078-497d-85ce-2bff7908841f
-function plot_state(x,xrange,t)
-	title = "t=$t   xₜ = $(round(x,digits=8))"
-	p1 = plot(xlims=xrange,ylims=(-0.1,0.1),size=(800,100),yaxis=false,yticks=false,title=title,legend=false)
-	plot!(p1,[xrange[1],xrange[2]],[0,0],c=:black)
-	#scatter!(p1,[x],[0],c=:red)
-end;
-
-# ╔═╡ d41c5669-adab-4954-9563-b0312dc5077d
-begin 
-	p1 = plot_state(m.x,(0,2.0),m.t)
-end;	
-
-# ╔═╡ d7b93817-efb6-4329-808e-95a360450184
-begin 
-	p2 = plot_state(m2.x,(0,1.0),m2.t)
-end;	
-
-# ╔═╡ 2f3f5e94-434e-46bc-94f2-0c4f8eda284d
-function plot_state!(p1,x,t)
-	title = "t=$t   xₜ = $(round(x,digits=8))"
-	scatter!(p1,[x],[0],c=:red,alpha=0.2)
-	title!(p1,title)
-end;
-
-# ╔═╡ 936816f9-5a79-474c-8847-96a1f0cb9a6a
-begin
-	n
-	m.x = R * m.x
-	m.t += 1
-	push!(m.xv,m.x)
-	p1b = plot(m.xv,marker=4,size=(800,300),xlabel="t",ylabel="xₜ",legend=false)
-	plot_state!(p1,m.x,m.t)
-end
-
-# ╔═╡ 4c273264-9c50-4f57-8136-b0756e116de0
-p1b
-
-# ╔═╡ cc09e3b4-8eca-40e0-a2ec-f8d58fe8cc68
-begin
-	n2
-	m2.x = R2 * m2.x * (1-m2.x)
-	m2.t += 1
-	push!(m2.xv,m2.x)
-	p2b = plot(m2.xv,size=(800,300),marker=4,xlabel="t",ylabel="xₜ",legend=false)
-	plot_state!(p2,m2.x,m2.t)
-end
-
-# ╔═╡ 20913b7d-7bfc-4795-9623-4b0f5770946c
-p2b
-
-# ╔═╡ f480e291-b67c-4a2f-9f44-914a340df81e
-html"""
-<style>
-input[type*="range"] {
-	width: 80%;
-}
-</style>
-"""
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -195,8 +64,9 @@ PlutoUI = "~0.7.39"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.3"
+julia_version = "1.8.0-rc1"
 manifest_format = "2.0"
+project_hash = "007fbb57db0277a809224fb92fc2c3ad5ea07613"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -212,6 +82,7 @@ version = "3.3.3"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -276,6 +147,7 @@ version = "4.1.0"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "0.5.2+0"
 
 [[deps.Contour]]
 deps = ["StaticArrays"]
@@ -316,6 +188,7 @@ version = "0.8.6"
 [[deps.Downloads]]
 deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
 
 [[deps.EarCut_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -531,10 +404,12 @@ version = "0.15.15"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "7.81.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -543,6 +418,7 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -627,6 +503,7 @@ version = "1.0.3"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.0+0"
 
 [[deps.Measures]]
 git-tree-sha1 = "e498ddeee6f9fdb4551ce855a46f54dbd900245f"
@@ -644,6 +521,7 @@ uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2022.2.1"
 
 [[deps.NaNMath]]
 git-tree-sha1 = "737a5957f387b17e74d4ad2f440eb330b39a62c5"
@@ -652,6 +530,7 @@ version = "1.0.0"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
 
 [[deps.Ogg_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -662,10 +541,12 @@ version = "1.3.5+1"
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.20+0"
 
 [[deps.OpenLibm_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "05823500-19ac-5b8b-9628-191a04bc5112"
+version = "0.8.1+0"
 
 [[deps.OpenSSL_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -711,6 +592,7 @@ version = "0.40.1+0"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.8.0"
 
 [[deps.PlotThemes]]
 deps = ["PlotUtils", "Statistics"]
@@ -790,6 +672,7 @@ version = "1.3.0"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
 
 [[deps.Scratch]]
 deps = ["Dates"]
@@ -861,6 +744,7 @@ version = "0.6.11"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.0"
 
 [[deps.TableTraits]]
 deps = ["IteratorInterfaceExtensions"]
@@ -877,6 +761,7 @@ version = "1.7.0"
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
 
 [[deps.TensorCore]]
 deps = ["LinearAlgebra"]
@@ -1069,6 +954,7 @@ version = "1.4.0+3"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.12+3"
 
 [[deps.Zstd_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -1085,6 +971,7 @@ version = "0.15.1+0"
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.1.0+0"
 
 [[deps.libfdk_aac_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -1107,10 +994,12 @@ version = "1.3.7+1"
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.41.0+1"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+0"
 
 [[deps.x264_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -1136,23 +1025,6 @@ version = "0.9.1+5"
 # ╟─18a83e00-0dd4-4fe7-a5be-644361f875d3
 # ╟─4ad5df20-f85d-11ec-3803-a7ed7ed8f2f3
 # ╟─157c155f-241e-4d39-93f7-35997d83191d
-# ╟─9490e7f8-668e-4acc-8efd-dcc4d355acb9
-# ╟─d9bb5aa8-2708-40c3-8d42-3b21e6d634c0
-# ╟─936816f9-5a79-474c-8847-96a1f0cb9a6a
-# ╟─dd1af6a9-c048-4845-8858-8051d23f6bee
-# ╟─4c273264-9c50-4f57-8136-b0756e116de0
-# ╠═d41c5669-adab-4954-9563-b0312dc5077d
-# ╠═ac175af3-0d5c-43ab-a41f-a5e6dfc1df72
-# ╟─226bcb3d-fdfa-484d-80da-2eb1e778017b
-# ╟─a31644b5-3aed-4ff8-9f46-0f8ff407f1b7
-# ╟─1115e7e3-06f2-4f51-a71d-85ada76ce3a9
-# ╟─cc09e3b4-8eca-40e0-a2ec-f8d58fe8cc68
-# ╟─20913b7d-7bfc-4795-9623-4b0f5770946c
-# ╟─d7b93817-efb6-4329-808e-95a360450184
-# ╟─7c3fb7dc-47f6-4a6c-b42a-76f74a5bc575
-# ╟─3a6821d7-392a-4dd4-b5c8-c247edf9132f
-# ╟─54918524-a078-497d-85ce-2bff7908841f
-# ╟─2f3f5e94-434e-46bc-94f2-0c4f8eda284d
-# ╟─f480e291-b67c-4a2f-9f44-914a340df81e
+# ╟─541330ac-2212-4681-8abb-f823fa5d1177
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
